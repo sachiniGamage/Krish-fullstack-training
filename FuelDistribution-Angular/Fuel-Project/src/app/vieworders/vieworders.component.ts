cb9fd066d7eacab64621dff4525c9a1485ecc2c4
sachiniGamage <harshika.weliwaththa@gmail.com>
2022-08-02 01:06:54 +0530

    fuel distribution- order - added two compoents

---
+++ FuelDistribution-Angular/Fuel-Project/src/app/vieworders/vieworders.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+
+@Component({
+  selector: 'em-vieworders',
+  templateUrl: './vieworders.component.html',
+  styleUrls: ['./vieworders.component.scss']
+})
+export class ViewordersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
